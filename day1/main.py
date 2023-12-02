48fc6c4c063191057d6a2a3d92d66f1f070d8faf
Łukasz Wójcik <lukasz.wojcik.22@gmail.com>
2023-12-02 23:10:19 +0100

    Add first two days

---
+++ day1/main.py
@@ -0,0 +1,28 @@
+import re
+
+
+spelled_letters = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
+
+with open('input.txt') as f:
+    lines = f.readlines()
+
+total = 0
+for line in lines:
+    line = line.strip()
+    for sp in spelled_letters.items():
+        line = line.replace(sp[0], sp[0][0] + sp[1] + sp[0][-1])
+
+    numbers = [number for number in re.findall(r'\d', line)]
+    total += int(numbers[0] + numbers[-1])
+
+print(f'The sum in the file:\n{total}')
---
+++ day2/main.py
@@ -0,0 +1,43 @@
+with open('input.txt', 'r') as f:
+    games = f.readlines()
+
+red_limit = 12
+green_limit = 13
+blue_limit = 14
+
+ids_sum = 0
+sets_power_sum = 0
+for game in games:
+    game_id = int(game.split(':')[0].split(' ')[1].replace(':', ''))
+    rounds = game.split(':')[1].split(';')
+
+    failed = False
+    r_min, g_min, b_min = 0, 0, 0
+    for rd in rounds:
+        r, g, b = 0, 0, 0
+        colors = rd.split(',')
+
+        for color in colors:
+            color = color.strip()
+            if 'red' in color:
+                r += int(color.split(' ')[0])
+            if 'green' in color:
+                g += int(color.split(' ')[0])
+            if 'blue' in color:
+                b += int(color.split(' ')[0])
+        
+        if r > red_limit or g > green_limit or b > blue_limit:
+            failed = True
+        
+        r_min = max(r_min, r)
+        g_min = max(g_min, g)
+        b_min = max(b_min, b)
+    
+    if not failed:
+        ids_sum += game_id
+    
+    game_power = r_min * g_min * b_min
+    sets_power_sum += game_power
+
+print(f'Sum of valid games IDs:\n{ids_sum}')
+print(f'Sum of the power of sets:\n{sets_power_sum}')
